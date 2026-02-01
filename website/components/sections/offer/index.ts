9a3b55fc0f15266921e2d5778937fd0e5f7b853f
david <davidel8698@gmail.com>
2026-02-01 21:03:49 +0200

    feat(16-02): create barrel export for offer section
    - Export GuaranteeBadge, GuaranteeBadgeProps, GuaranteeType
    - Export ZeroRiskSummary, ZeroRiskSummaryProps
    - Enable clean imports from @/components/sections/offer

---
+++ website/components/sections/offer/index.ts
@@ -0,0 +1,6 @@
+export {
+  GuaranteeBadge,
+  type GuaranteeBadgeProps,
+  type GuaranteeType,
+} from "./GuaranteeBadges";
+export { ZeroRiskSummary, type ZeroRiskSummaryProps } from "./ZeroRiskSummary";
