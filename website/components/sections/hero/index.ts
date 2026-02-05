b353c7fc5314302695d507658aa569156eaf1b75
david <davidel8698@gmail.com>
2026-02-05 17:04:53 +0200

    feat(26-03): export GlassNav from hero index
    - Make GlassNav available for import from hero section

--- website/components/sections/hero/index.ts
+++ website/components/sections/hero/index.ts
@@ -4,5 +4,6 @@ export { ActivityFeed } from "./ActivityFeed";
 export { HeroContent } from "./HeroContent";
 export { TrustSignal } from "./TrustSignal";
 export { StickyCtaBar } from "./StickyCtaBar";
+export { GlassNav } from "./GlassNav";
 export { Hero } from "./Hero";
 export { Hero as default } from "./Hero";
